fb4c41fce0cb086bd3d89637c17d03052f18a4ca
Angga Ari Wijaya <anggadarkprince@gmail.com>
2017-07-02 10:19:52 +0700

    fining login page

--- views/account/login.php
+++ views/account/login.php
@@ -6,42 +6,68 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Login - Yummy';
+$this->params['banner-class'] = 'banner-featured banner-2';
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row">
+        <div class="col-md-4">
+            <h1 class="lead">Sign In With Existing Email</h1>
 
-    <p>Please fill out the following fields to login:</p>
+            <p>Please fill out the following fields to login</p>
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
+            <?php $form = ActiveForm::begin([
+                'id' => 'login-form',
+            ]); ?>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'username')->textInput([
+                'placeholder' => 'Put username or email'
+            ]) ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'password')->passwordInput([
+                'placeholder' => 'Password'
+            ]) ?>
 
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <div class="form-group">
+                <?= Html::submitButton('Login', [
+                    'class' => 'btn btn-primary btn-block',
+                    'name' => 'login-button'
+                ]) ?>
             </div>
+
+            <?php ActiveForm::end(); ?>
+
+            <p class="small">
+                By signing in, you are agreeing to our <a href="<?= Url::to('/legal/tos') ?>">Terms of
+                    Service</a> and our <a href="<?= Url::to('/legal/privacy') ?>">Privacy Policy Rights</a>.
+            </p>
+
         </div>
+        <div class="col-md-8 col-lg-offset-1 col-lg-7">
+            <div class="promotion">
+                <h1>Yummy,
+                    <small>where delicious recipe come from.</small>
+                </h1>
+                <p>Join with us, along people around to make the world sweet and yummy place.</p>
+            </div>
 
-    <?php ActiveForm::end(); ?>
+            <div class="social">
+                <p class="lead">Sign in with social.</p>
+                <p>New and existing Yummy users.</p>
+                <button class="btn btn-lg btn-primary btn-social"><i class="fa fa-facebook"></i>Facebook</button>
+                <button class="btn btn-lg btn-info btn-social"><i class="fa fa-twitter"></i>Twitter</button>
+                <button class="btn btn-lg btn-danger btn-social"><i class="fa fa-google"></i>Google</button>
 
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+            </div>
+            <p>
+                Get trouble with login, try <a href="<?= Url::to('/account/forgot') ?>">Forgot Password?</a>
+                <br>
+                Or <a href="<?= Url::to('/account/register') ?>">Register new account?</a>
+            </p>
+        </div>
     </div>
 </div>
